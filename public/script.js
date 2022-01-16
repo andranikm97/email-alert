43161aef92a2909bd9ff4cf286624ea02b064bbd
Andranik Markaryan <andranik.mrk@gmail.com>
2022-01-16 01:05:20 +0000

    Feat: move CSS injection to JS

--- public/script.js
+++ public/script.js
@@ -31,11 +31,150 @@ const modalSkeleton = `
         </div>
       </form>
   `;
+
 let modal = document.createElement('div');
 modal.classList.add('email-modal');
 modal.id = 'email-modal';
 modal.innerHTML = modalSkeleton;
 document.body.appendChild(modal);
+
+const styles = `html,
+body {
+  margin: 0;
+  height: 100%;
+}
+
+body {
+  width: 100%;
+  background-color: #fefbf3;
+  padding: 15px;
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: relative;
+}
+
+.content {
+  flex: 10;
+  margin-top: 15px;
+  width: 70%;
+  background-color: white;
+  border-radius: 5px;
+  border-width: 1px;
+  border-style: solid;
+  border-color: black;
+  overflow: scroll;
+  padding: 10px;
+
+  -ms-overflow-style: none; /* IE and Edge */
+  scrollbar-width: none; /* Firefox */
+}
+
+.content::-webkit-scrollbar {
+  display: none; /*Chrome*/
+}
+
+.randomNum {
+  flex: 1;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  visibility: visible;
+  width: 100%;
+  font-size: 30px;
+}
+
+.email-modal {
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 1;
+  height: 100vh;
+  width: 100%;
+  display: none;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  background-color: rgba(0, 0, 0, 0.4);
+}
+
+.modal-box {
+  position: relative;
+  padding: 30px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  max-width: 60%;
+  border-radius: 5px;
+  background-color: antiquewhite;
+}
+
+.checkmark {
+  width: 40px;
+}
+
+.close-button {
+  position: absolute;
+  top: 5px;
+  right: 5px;
+  width: 20px;
+}
+
+.close-button:hover {
+  cursor: pointer;
+}
+
+.input-box {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.email-submitted-box {
+  display: none;
+  position: relative;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.invalid-email-tooltip {
+  visibility: hidden;
+  width: 120px;
+  background-color: black;
+  color: #fff;
+  text-align: center;
+  border-radius: 6px;
+  padding: 5px 0;
+  position: absolute;
+  z-index: 1;
+  top: 40px;
+  left: 105%;
+
+  opacity: 0;
+  transition: opacity 0.5s;
+}
+
+.invalid-email-tooltip::after {
+  content: ' ';
+  position: absolute;
+  top: 50%;
+  right: 100%; /* To the left of the tooltip */
+  margin-top: -5px;
+  border-width: 5px;
+  border-style: solid;
+  border-color: transparent black transparent transparent;
+}
+`;
+
+let stylesheet = document.createElement('style');
+stylesheet.innerHTML = styles;
+document.head.appendChild(stylesheet);
 
 let randomNumberText = document.getElementById('ran');
 let randomNumberBox = document.getElementById('random-number-box');
